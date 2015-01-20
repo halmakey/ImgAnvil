dc20aed074aef4b9f80afd151fc05e77d7ba8358
halmakey <halmakey@cubicplus.net>
2015-01-20 19:21:28 +0900

    initial

---
+++ src/main/java/NinePatcher.java
@@ -0,0 +1,94 @@
+import jp.rubi3.ninepatcher.Patcher;
+import org.apache.commons.cli.*;
+import org.apache.commons.io.FilenameUtils;
+import static java.lang.System.out;
+
+import java.io.File;
+import java.util.stream.IntStream;
+
+/**
+ * Created by kikuchi on 2015/01/20.
+ */
+public class NinePatcher {
+    private static final String OPTION_IN_FILE = "i";
+    private static final String OPTION_STRETCHABLE_AREA_PADDING = "s";
+    private static final String OPTION_CONTENT_PADDING = "c";
+
+    public static void main(String[] args) {
+
+        Options options = new Options();
+
+        {
+            options.addOption(OptionBuilder.withArgName("path")
+                    .hasArg()
+                    .withDescription("入力ファイル名")
+                    .isRequired()
+                    .create(OPTION_IN_FILE));
+
+            options.addOption(OptionBuilder.withArgName("left top right bottom")
+                    .hasArgs(4)
+                    .withDescription("Padding for Stretchable area.")
+                    .create(OPTION_STRETCHABLE_AREA_PADDING));
+
+            options.addOption(OptionBuilder.withArgName("left top right bottom")
+                    .hasArgs(4)
+                    .withDescription("Padding for Content area.")
+                    .create(OPTION_CONTENT_PADDING));
+
+        }
+        CommandLineParser parser = new PosixParser();
+        CommandLine cmd = null;
+
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            HelpFormatter help = new HelpFormatter();
+            help.printHelp(NinePatcher.class.getSimpleName(), options, true);
+            return;
+        }
+
+        String inFile = cmd.getOptionValue(OPTION_IN_FILE);
+        String extension = FilenameUtils.getExtension(inFile);
+        String outFile = FilenameUtils.removeExtension(inFile);
+        outFile += ".9." + extension;
+
+        Patcher patcher = new Patcher(new File(inFile));
+        
+        if (cmd.hasOption(OPTION_STRETCHABLE_AREA_PADDING)) {
+            String values[] = cmd.getOptionValues(OPTION_STRETCHABLE_AREA_PADDING);
+            int edges[] = new int[args.length];
+            IntStream.range(0, values.length).forEach(i -> {
+                try {
+                    edges[i] = Integer.parseInt(values[i]);
+                } catch (NumberFormatException e) {
+                    edges[i] = 0;
+                }
+            });
+            patcher.padding(edges[0], edges[1], edges[2], edges[3]);
+        } else {
+            patcher.padding(0, 0, 0, 0);
+        }
+        
+        if (cmd.hasOption(OPTION_CONTENT_PADDING)) {
+            String values[] = cmd.getOptionValues(OPTION_CONTENT_PADDING);
+            int edges[] = new int[args.length];
+            IntStream.range(0, values.length).forEach(i -> {
+                try {
+                    edges[i] = Integer.parseInt(values[i]);
+                } catch (NumberFormatException e) {
+                    edges[i] = 0;
+                }
+            });
+            patcher.content(edges[0], edges[1], edges[2], edges[3]);
+        } else {
+            patcher.content(0, 0, 0, 0);
+        }
+
+        if (patcher.saveTo(new File(outFile)).isSuccessful()) {
+            out.println(outFile + " success");
+        } else {
+            out.println(outFile + " failed " + patcher.getException().getLocalizedMessage());
+        }
+        
+    }
+}
---
+++ src/main/java/jp/rubi3/ninepatcher/Patcher.java
@@ -0,0 +1,79 @@
+package jp.rubi3.ninepatcher;
+
+import lombok.Getter;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by kikuchi on 2015/01/20.
+ */
+public class Patcher {
+    private BufferedImage base;
+    @Getter
+    private BufferedImage canvas;
+    private Graphics2D graphics;
+    @Getter
+    private Exception exception;
+
+    public Patcher(File file) {
+        try {
+            base = ImageIO.read(file);
+            canvas = new BufferedImage(base.getWidth() + 2, base.getHeight() + 2, BufferedImage.TYPE_INT_ARGB);
+            graphics = canvas.createGraphics();
+
+            graphics.drawImage(base, 1, 1, null);
+            
+            exception = null;
+            
+        } catch (IOException e) {
+            exception = e;
+            e.printStackTrace();
+        }
+    }
+    
+    public Patcher content(int left, int top, int right, int bottom) {
+        int minX = 1;
+        int minY = 1;
+        int maxX = canvas.getWidth() - 2;
+        int maxY = canvas.getHeight() - 2;
+        int infoY = canvas.getHeight() - 1;
+        int infoX = canvas.getWidth() - 1;
+        
+        graphics.setColor(Color.black);
+        graphics.drawLine(minX + left, infoY, maxX - right, infoY);
+        graphics.drawLine(infoX, minY + top, infoX, maxY - bottom);
+        return this;
+    }
+
+    public Patcher padding(int left, int top, int right, int bottom) {
+        int minX = 1;
+        int minY = 1;
+        int maxX = canvas.getWidth() - 2;
+        int maxY = canvas.getHeight() - 2;
+        int infoY = 0;
+        int infoX = 0;
+
+        graphics.setColor(Color.black);
+        graphics.drawLine(minX + left, infoY, maxX - right, infoY);
+        graphics.drawLine(infoX, minY + top, infoX, maxY - bottom);
+        return this;
+    }
+    
+    public Patcher saveTo(File file) {
+        try {
+            ImageIO.write(canvas, "png", file);
+        } catch (IOException e) {
+            exception = e;
+            e.printStackTrace();
+        }
+        return this;
+    }
+    
+    public boolean isSuccessful() {
+        return exception == null;
+    }
+}
